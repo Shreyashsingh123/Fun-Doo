508722ea88c3f914219287cba5c3b9e669073fab
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-01-28 10:57:14 +0530

    Added SideBar.jsx , Created SideBar page

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -11,21 +11,28 @@ import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import Toolbar from '@mui/material/Toolbar';
-import { useState } from 'react';
 import { useDrawer } from './Side-Bar-Context'
+import { useNavigate,useLocation } from 'react-router-dom';
+
 
 const menuItems = [
-  { text: 'Notes', icon: LightbulbOutlinedIcon },
-  { text: 'Reminder', icon: NotificationsNoneOutlinedIcon },
-  { text: 'Edit Labels', icon: EditOutlinedIcon },
-  { text: 'Archive', icon: ArchiveOutlinedIcon },
-  { text: 'Trash', icon: DeleteOutlinedIcon },
+  { text: 'Notes', icon: LightbulbOutlinedIcon ,link:'/'},
+  { text: 'Reminder', icon: NotificationsNoneOutlinedIcon , link:'/reminder' },
+  { text: 'Edit_Labels', icon: EditOutlinedIcon, link:'/edit_labels'},
+  { text: 'Archive', icon: ArchiveOutlinedIcon , link:'/archive' },
+  { text: 'Trash', icon: DeleteOutlinedIcon, link:'/trash' },
 ];
 
 
 export default function Sidebar() {
     const { open } = useDrawer();
-    let drawerWidth = open?270:72;
+    let drawerWidth = open?250:72;
+const handleClick =(text)=>{
+navigate(text==='Notes' ? '/' : `/${text.toLowerCase()}`);
+}
+    const navigate = useNavigate();
+    const loaction=useLocation();
+
 
   return (
     <>
@@ -44,24 +51,28 @@ export default function Sidebar() {
         <Toolbar />
         <Box sx={{ overflow: 'auto' }}>
           <List>
-            {menuItems.map((item, index) => (
+            {menuItems.map((item, index) => {
+              const isActive = loaction.pathname === (item.link);
+              return (
+
               <ListItem
                 key={item.text}
                 disablePadding
                 sx={{
-                  backgroundColor: index === 0 ? '#feefc3' : 'transparent',
+                  backgroundColor: (isActive) ? '#feefc5' : 'transparent',
                   borderTopRightRadius: '50px',
                   borderBottomRightRadius: '50px',
                 }}
               >
-                <ListItemButton>
-                  <ListItemIcon sx={{ minWidth: 48 }}> {/* Better spacing for icons */}
+                <ListItemButton onClick={() => handleClick(item.text)}>
+                  <ListItemIcon sx={{ minWidth: 48 }}> 
                     <item.icon />
                   </ListItemIcon>
                   <ListItemText primary={item.text} sx={{display:open?'block':'none'}}  />
                 </ListItemButton>
               </ListItem>
-            ))}
+              )
+})}
           </List>
         </Box>
       </Drawer>
