4565e389bce9b46734a2ecdb9e559daada28d93b
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-01-28 10:55:06 +0530

    Added Side-Bar-Context.jsx , Created a SideBar page page

--- src/Components/Side-Bar-Context.jsx
+++ src/Components/Side-Bar-Context.jsx
@@ -3,14 +3,20 @@ import { createContext, useContext, useState } from 'react'
 const DrawerContext = createContext()
 
 export const DrawerProvider = ({ children }) => {
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(true);
+  const [click, setclick] = useState(true);
+  const handlepattern = () => {
+    setclick(prev => !prev)
+
+  }
+
 
   const toggleDrawer = () => {
     setOpen(prev => !prev)
   }
 
   return (
-    <DrawerContext.Provider value={{ open, toggleDrawer }}>
+    <DrawerContext.Provider value={{ open, toggleDrawer,click,handlepattern}}>
       {children}
     </DrawerContext.Provider>
   )
