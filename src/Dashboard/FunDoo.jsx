0a743bcf11720f2794c5b247d120fff76eb4cb3f
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-01-28 11:00:40 +0530

    Added FunDoo.jsx , Created FunDoo page

--- src/Dashboard/FunDoo.jsx
+++ src/Dashboard/FunDoo.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Header from '../Components/Header'
 import Sidebar from '../Components/SideBar'
 import { DrawerProvider } from '../Components/Side-Bar-Context'
-import Poper from '../Components/Poper'
+// import Poper from '../Components/Poper'
+import Notes from '../Components/Notes'
+import {Outlet} from 'react-router-dom';
 function FunDoo() {
   return (
     <>
     <DrawerProvider>
     <Header/>
     <Sidebar/>
+   <Outlet/>
+    
     </DrawerProvider>
   
     </>
