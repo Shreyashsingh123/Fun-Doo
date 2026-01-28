3fe8422196c2b2865de718dbd17757f44de31475
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-01-28 10:52:23 +0530

    Added Poper.jsx , Created a Poper for Account Section

--- src/Components/Poper.jsx
+++ src/Components/Poper.jsx
@@ -2,30 +2,32 @@ import React from 'react'
 import { useState } from 'react';
 import {Card}from '@mui/material';
 import Popper from '@mui/material/Popper';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+
 import Box from '@mui/material/Box'
 import { Avatar, ClickAwayListener } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import {Divider} from '@mui/material';
-import { useNavigate,Link } from "react-router-dom";
 
+import Button from '@mui/material/Button';
+import { useNavigate,Link } from "react-router-dom";
+import AddIcon from '@mui/icons-material/Add';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 function Poper({anchorEl,onclose}) {
     const open=Boolean(anchorEl);
   return (
-   <Popper open={open} anchorEl={anchorEl} disablePortal>
+   <Popper open={open} anchorEl={anchorEl} disablePortal sx={{zIndex:1300}}>
     <ClickAwayListener onClickAway={onclose}>
   
     <Card
           sx={{
-            width: 400,
+            width: 390,
+            height:280,
             mt: 3,
             mr:3,
             p: 2,
             borderRadius: 5,
-            boxShadow: 5,
+            boxShadow: 2,
+            backgroundColor:"lightgray",
           }}
     >
         <Box sx={{display:"flex",justifyContent:'center'}}>
@@ -41,12 +43,17 @@ function Poper({anchorEl,onclose}) {
           shreyash18@gmail.com
           </Typography>
 
-         <Divider sx={{my:2}}/>
-    <div style={{display:"flex",flexDirection:"row",gap:'2px'}}>
-    <Button   component={Link} to="/signin" sx={{borderRadius:2,height:'55px'}}fullWidth variant="outlined">Add Account</Button>
-        <Button  sx={{borderRadius:2}} fullWidth variant="outlined">SignOut</Button>
+         <Divider sx={{my:3}}/>
+    <div style={{display:"flex",flexDirection:"row",gap:'2px',mt:2}}>
+    <Button   component={Link} to="/signin" sx={{borderBottomLeftRadius:25,borderTopLeftRadius:25, height:'55px',backgroundColor:'white',fontsize:'15px',color:'black'}}fullWidth variant="outlined">
+      <AddIcon/>
+      Add Account</Button>
+        <Button  sx={{borderTopRightRadius:25,borderBottomRightRadius:25,backgroundColor:'white',fontsize:'15px',color:'black'}} fullWidth variant="outlined">
+          <LogoutOutlinedIcon/>
+          SignOut</Button>
     </div>
     </Card>
+
     </ClickAwayListener>
     </Popper>
   )
